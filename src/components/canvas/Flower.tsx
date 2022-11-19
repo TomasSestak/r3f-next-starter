10f17e6b5a5b2c537da7def6dad8a73359813ff2
sadboi <tomassestak@seznam.cz>
2022-11-19 22:10:16 +0100

    add tailwind and cloning models

--- src/components/canvas/Flower.tsx
+++ src/components/canvas/Flower.tsx
@@ -1,12 +1,55 @@
-import { useLoader } from '@react-three/fiber';
+import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useDrag } from '@use-gesture/react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import { useGesture } from '@use-gesture/react';
+import { useSpring } from '@react-spring/three';
+import { OrbitControls, PresentationControls, TransformControls, Bounds } from '@react-three/drei';
 
-const Flower = () => {
+import { animated } from '@react-spring/three';
+import { Group } from 'three';
+
+const Single = (props) => {
 	const { scene } = useLoader(GLTFLoader, '/3d-models/poly.glb');
+	return <primitive object={scene} {...props} />;
+};
+
+export interface FlowerState {
+	flowers: number;
+	setFlowers: Dispatch<SetStateAction<number>>;
+}
+
+const Flower = ({ flowers, setFlowers }: FlowerState) => {
+	const { size, viewport } = useThree();
+	const aspect = size.width / viewport.width;
+	const [spring, set] = useSpring(() => ({ scale: [1, 1, 1], position: [0, 0, 0], rotation: [0, 0, 0], config: { friction: 10 } }));
+	const bind = useGesture({
+		onDrag: ({ offset: [x, y] }) => set({ position: [x / aspect, -y / aspect, 0], rotation: [y / aspect, x / aspect, 0] }),
+	});
+
+	const { scene } = useLoader(GLTFLoader, '/3d-models/flower.gltf');
+
+	const [scenes, setScenes] = useState<Group[]>([]);
+
+	useEffect(() => {
+		const newScenes = Array(flowers)
+			.fill(0)
+			.map(() => {
+				return scene.clone();
+			});
+		setScenes(newScenes);
+	}, [flowers, scene]);
+
+	console.log(scenes);
 
 	return (
 		<>
-			<primitive object={scene} scale={10} />
+			<OrbitControls makeDefault />
+			<group scale={5}>
+				{scenes.map((value, index) => {
+					return <primitive object={value} key={index} position={[index / 20, 0, 0]} />;
+				})}
+			</group>
 			<ambientLight />
 		</>
 	);
--- src/pages/flower.tsx
+++ src/pages/flower.tsx
@@ -1,29 +1,38 @@
 import dynamic from 'next/dynamic';
 import useStore from '@/helpers/store';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { FlowerState } from '@/components/canvas/Flower';
 
 const Flower = dynamic(() => import('@/components/canvas/Flower'), {
 	ssr: false,
 });
 
 // DOM elements here
-const DOM = () => {
-	return <></>;
+const DOM = ({ setFlowers, flowers }: FlowerState) => {
+	console.log(flowers);
+	return (
+		<button onClick={() => setFlowers((flowers) => flowers + 1)} className={'mx-auto bg-orange-400 grid mt-8'}>
+			Add flower
+		</button>
+	);
 };
 
 // Canvas/R3F components here
-const R3F = () => {
+const R3F = ({ setFlowers, flowers }: FlowerState) => {
 	return (
 		<>
-			<Flower />
+			<Flower setFlowers={setFlowers} flowers={flowers} />
 		</>
 	);
 };
 
 export default function Page() {
+	const [flowers, setFlowers] = useState(1);
+
 	return (
 		<>
-			<DOM />
-			<R3F />
+			<DOM flowers={flowers} setFlowers={setFlowers} />
+			<R3F flowers={flowers} setFlowers={setFlowers} />
 		</>
 	);
 }
